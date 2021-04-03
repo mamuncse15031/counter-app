73099c18b6f2594ec833ec540b0e85acd65680d9
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-04-03 19:02:55 +0600

    Updating State using setState

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,7 +21,7 @@ class Counter extends Component {
 
   handleIncrement = () => {
     //event Handler, Biding Event Handler
-    console.log('Increment Done', this);
+    this.setState({ count: this.state.count + 1 }); //Updating State
   };
 
   render() {
