b3801fd2739da72e1e4548b7631459b5d8c2bb70
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-04-03 10:12:51 +0600

    First Commit
    Creationg Local Repository.
    Here I am going to learn about components part by part.
    Topics are: embedding expression, event handling, setting attributes, conditional rendering, Binding Event Handler etc.

---
+++ src/components/counter.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
+  };
+
+  renderTags = () => {
+    //Conditional Rendering
+    if (this.state.tags.length === 0) return <p>There are no Tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag} </li> //rendering lists
+        ))}
+      </ul>
+    );
+  };
+
+  handleIncrement = () => {
+    //event Handler, Biding Event Handler
+    console.log('Increment Done', this);
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          increment
+        </button>
+        {this.renderTags()};
+      </div>
+    );
+  }
+
+  //expression embedding
+  formatCounter = () => {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  };
+
+  //rendering classes dynamically
+  getBadgeClasses = () => {
+    let classes = 'badge m-2 badge-';
+    classes += this.state.count === 0 ? 'warning' : 'primary';
+    return classes;
+  };
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
