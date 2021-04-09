93e6101a5fb1a4d19643c54ae05b2a338bbdb607
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-04-09 14:22:32 +0600

    nothing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,9 +6,9 @@ class Navbar extends Component {
     return (
       <nav className='navbar navbar-light bg-light'>
         <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>
-            Navbar
-          </a>
+          {/* <a className='navbar-brand' href='#'> */}
+          Navbar
+          {/* </a> */}
         </div>
       </nav>
     );
