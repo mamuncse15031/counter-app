7db4a85b145608c8a1155b873a021d8ba08c93a1
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-04-09 08:41:56 +0600

    Single Source of truth, Removing the local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    // tags: ['tag1', 'tag2', 'tag3'],
-  };
-
-  //passing event arguments
-  handleIncrement = (product) => {
-    //event Handler, Biding Event Handler
-    this.setState({ value: this.state.value + 1 }); //Updating State
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
 
         <button
-          onClick={() => {
-            this.handleIncrement({ id: 1 });
-          }}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className='btn btn-secondary btn-sm'
         >
           increment
@@ -39,29 +26,16 @@ class Counter extends Component {
 
   //expression embedding
   formatCounter = () => {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'Zero' : count;
   };
 
   //rendering classes dynamically
   getBadgeClasses = () => {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   };
-
-  // renderTags = () => {
-  //   //Conditional Rendering
-  //   if (this.state.tags.length === 0) return <p>There are no Tags</p>;
-
-  //   return (
-  //     <ul>
-  //       {this.state.tags.map((tag) => (
-  //         <li key={tag}>{tag} </li> //rendering lists
-  //       ))}
-  //     </ul>
-  //   );
-  // };
 }
 
 export default Counter;
